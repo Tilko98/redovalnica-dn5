c6c1aa437baa6c593c5b548a15497a35440f1a1d
Tilko98 <knez.tilen@gmail.com>
2025-11-23 10:07:27 +0100

    Initial commit - DN5 redovalnica

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+	"github.com/Tilko98/redovalnica-dn5/redovalnica"
+
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:  "redovalnica",
+		Usage: "upravljanje ocen študentov",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:  "stOcen",
+				Usage: "minimalno število ocen za pozitivno oceno",
+				Value: 6,
+			},
+			&cli.IntFlag{
+				Name:  "minOcena",
+				Usage: "najmanjša dovoljena ocena",
+				Value: 1,
+			},
+			&cli.IntFlag{
+				Name:  "maxOcena",
+				Usage: "največja dovoljena ocena",
+				Value: 10,
+			},
+		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			// preberemo vrednosti flagov
+			stOcen := cmd.Int("stOcen")
+			minOcena := cmd.Int("minOcena")
+			maxOcena := cmd.Int("maxOcena")
+
+			// testni študenti
+			studenti := map[string]redovalnica.Student{
+				"63210001": {Ime: "Ana", Priimek: "Novak", Ocene: []int{10, 9, 8}},
+				"63210002": {Ime: "Boris", Priimek: "Kralj", Ocene: []int{6, 7, 5, 8}},
+				"63210003": {Ime: "Janez", Priimek: "Novak", Ocene: []int{4, 5, 3, 5}},
+			}
+
+			// uporabiš funkcije iz paketa redovalnica
+			redovalnica.DodajOceno(studenti, "63210001", 10, minOcena, maxOcena)
+			redovalnica.IzpisVsehOcen(studenti)
+			redovalnica.IzpisiKoncniUspeh(studenti, stOcen)
+
+			fmt.Println("CLI se je uspešno izvedel.")
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ redovalnica/redovalnica.go
@@ -0,0 +1,73 @@
+package redovalnica
+
+import "fmt"
+
+type Student struct {
+Ime     string
+Priimek string
+Ocene   []int
+}
+
+func DodajOceno(studenti map[string]Student, vpisnaStevilka string, ocena int, minOcena int, maxOcena int) {
+    // Preveri, ali je ocena v ustreznem območju
+    if ocena < minOcena || ocena > maxOcena {
+        fmt.Printf("Napaka: Ocena %d ni v veljavnem območju (0-10)\n", ocena)
+        return
+    }
+    
+    // Preveri, ali študent obstaja
+    student, obstaja := studenti[vpisnaStevilka]
+    if !obstaja {
+        fmt.Println("Napaka: Študent z vpisno številko", vpisnaStevilka, "ne obstaja na seznamu")
+        return
+    }
+    
+    // Dodaj oceno študentu
+    student.Ocene = append(student.Ocene, ocena)
+    studenti[vpisnaStevilka] = student
+    //fmt.Printf("Študentu %s %s je bila dodana ocena %d\n", student.ime, student.priimek, ocena)
+}
+
+func povprecje(studenti map[string]Student, vpisnaStevilka string, stOcen int) float64 {
+    student, obstaja := studenti[vpisnaStevilka]
+    if !obstaja {
+        fmt.Println("Napaka: Študent z vpisno številko", vpisnaStevilka, "ne obstaja na seznamu")
+        return -1.0
+    }
+
+    if len(student.Ocene) < stOcen{
+        return 0.0
+    }
+
+    suma := 0
+    for _, ocena := range student.Ocene {
+        suma += ocena
+    }
+
+    avg := float64(suma) / float64(len(student.Ocene))
+    if avg < 6.0 {
+        return 0.0
+    }
+    return avg
+
+}
+
+func IzpisVsehOcen(studenti map[string]Student) {
+    fmt.Println("Redovalnica:")
+    for vpisnaStevilka, s := range studenti{
+        fmt.Printf("%s - %s %s: %v\n", vpisnaStevilka, s.Ime, s.Priimek, s.Ocene)
+    }
+}
+
+func IzpisiKoncniUspeh(studenti map[string]Student, stOcen int) {
+    for vpisnaStevilka, s := range studenti {
+        avg := povprecje(studenti, vpisnaStevilka, stOcen)
+        if avg >= 9 {
+            fmt.Printf("%s %s: povprečna ocena %.2f -> Odličen študent!\n", s.Ime, s.Priimek, avg)
+        } else if avg >= 6 && avg < 9 {
+            fmt.Printf("%s %s: povprečna ocena %.2f -> Povprečen študent\n", s.Ime, s.Priimek, avg)
+        } else {
+            fmt.Printf("%s %s: povprečna ocena %.2f -> Neuspešen študent\n", s.Ime, s.Priimek, avg)
+        }
+    }
+}
