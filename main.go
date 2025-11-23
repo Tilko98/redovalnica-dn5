33e70b92bafc48e0b98daab9f59efaf07a203f88
Tilko98 <knez.tilen@gmail.com>
2025-11-23 10:13:11 +0100

    Sprememba izpisa

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 
@@ -50,7 +49,7 @@ func main() {
 			redovalnica.IzpisVsehOcen(studenti)
 			redovalnica.IzpisiKoncniUspeh(studenti, stOcen)
 
-			fmt.Println("CLI se je uspešno izvedel.")
+			//fmt.Println("CLI se je uspešno izvedel.")
 			return nil
 		},
 	}
